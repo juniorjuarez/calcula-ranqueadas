ec5a03413a23b75b07b1ccfae9ef926f578f167e
Jose Juarez <juniojuarez@yahoo.com.br>
2023-11-01 06:00:17 -0300

    unique commit

---
+++ calcula-ranckeadas/Program.cs
@@ -0,0 +1,26 @@
+﻿using calcula_ranckeadas;
+using System;
+
+namespace clacula_ranckeadas {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=============================================================================");
+            Console.WriteLine("========================= Calculadora de ranqueadas =========================");
+            Console.WriteLine("Informe a quantidade de vitorias e de derrotas! ");
+            Console.Write("Vitorias: ");
+            int vitoria = int.Parse(Console.ReadLine());
+
+            Console.Write("Derrotas: ");
+            int derrotas = int.Parse(Console.ReadLine());
+
+            Usuario_ranqueada.calculaRanqueadas(vitoria, derrotas);
+
+
+        }
+    }
+    
+    }
---
+++ calcula-ranckeadas/Usuario-ranqueada.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace calcula_ranckeadas
+{
+    internal class Usuario_ranqueada
+    {
+        public static void calculaRanqueadas(int a, int b)
+        {
+            int result = a - b;
+
+            if (result > 0)
+            {
+                Console.WriteLine($"Seu saldo é positivo, total de vitorias maior que de derrotas, resultado: {result}");
+            }
+            else if (result == 0)
+            {
+                Console.WriteLine("Resultado zerado, total de vitorias igual a de derrotas");
+
+            }
+            else {
+               
+                Console.WriteLine($"Resultado negativo, valor: {result}");
+            }
+            
+        }
+    }
+}
